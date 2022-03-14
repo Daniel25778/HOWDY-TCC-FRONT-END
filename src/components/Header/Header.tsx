cde8c07814536cc91809c13d3197c6ec8b8c340b
Daniel25778 <danielwilsonca94@gmail.com>
2022-03-14 11:29:29 -0300

    header almost finalized

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,27 +1,55 @@
-import { Flex } from "@chakra-ui/react";
-
+import { useColorMode } from "@chakra-ui/core";
+import { Button, Center, color, Flex, Image, Text } from "@chakra-ui/react";
+import '@fontsource/roboto/400.css';
 
 
 export function HeaderWeb () {
+
     return(
-    
-       <Flex 
-       as="header" 
-       w="100%"
-       maxWidth={2550}
-       h="20"
-       mx="auto"
-       mt="4"
-       px="6"
-       align="center"
-       bg="howdyColors.mainBlue"
-       >
+    <>
+     <Flex 
+        position={"relative"}
+        as="header" 
+        w="100%"
+        maxWidth={3000}
+        align={"center"}
+        h={150}
+        justify="center"
+        bg="howdyColors.mainBlue"
+        flexDir="column"
+        >
 
+        <Text>HELLO</Text>    
 
+        <Flex
+            position={"absolute"}
+            w="60%"
+            h="20"
+            bg={"gray.100"}
+            borderRadius={10}
+            boxShadow='lg'
+            paddingX={20}
+            justify={"space-between"}
+        >
 
+        <Image 
+            width={200}
+            maxWidth={400}
+            src="/images/howdy-images/logo/Logo row.svg"
+            alt="howdy logo">
+        </Image>
 
-       </Flex>
+        <Center w="50%" justifyContent={"space-between"}>
+            <Text _hover={{color: "howdyColors.mainBlue" }} cursor={"pointer"} color="howdyColors.mainBlack" fontSize={20}>Ranking</Text>
+            <Text _hover={{color: "howdyColors.mainBlue" }} cursor={"pointer"} color="howdyColors.mainBlack" fontSize={20}>Postagens</Text>
+            <Text _hover={{color: "howdyColors.mainBlue" }} cursor={"pointer"} color="howdyColors.mainBlack" fontSize={20}>Aprenda</Text>
+        </Center>
+           
 
+        </Flex>
+     </Flex>
+
+    </>
 
     );
 
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -53,4 +53,16 @@ export const themes = extendTheme({
             },
         },
     },
+    layerStyles: {
+        base: {
+            bg: 'gray.50',
+            border: '2px solid',
+            borderColor: 'gray.500',
+          },
+        selected: {
+          bg: 'teal.500',
+          color: 'teal.700',
+          borderColor: 'orange.500',
+        },
+      },
 });

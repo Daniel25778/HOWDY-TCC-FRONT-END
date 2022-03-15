ca01a66497821dc372a9417889aa0c725f383ef4
GabrielTSR <gabrielalvesrocha22@gmail.com>
2022-03-15 07:46:44 -0300

    header updated

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,56 +1,68 @@
-import { useColorMode } from "@chakra-ui/core";
-import { Button, Center, color, Flex, Image, Text } from "@chakra-ui/react";
+import { useColorMode } from '@chakra-ui/core';
+import { Button, Center, color, Flex, Image, Text } from '@chakra-ui/react';
 import '@fontsource/roboto/400.css';
 
+export function HeaderWeb() {
+    return (
+        <>
+            <Flex
+                position="fixed"
+                as="header"
+                top="0"
+                left="0"
+                w="100%"
+                justify="center"
+                h="5rem"
+                bg="howdyColors.mainBlue"
+            >
+                <Flex
+                    position="absolute"
+                    bottom="-2rem"
+                    w="60%"
+                    h="4rem"
+                    bg="gray.100"
+                    borderRadius="10px"
+                    boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
+                    paddingX={20}
+                    justify="space-between"
+                    align="center"
+                >
+                    <Image height="3rem" src="/images/howdy-images/logo/Logo row.svg" alt="howdy logo"></Image>
 
-export function HeaderWeb () {
-
-    return(
-    <>
-     <Flex 
-        position={"relative"}
-        as="header" 
-        w="100%"
-        maxWidth={3000}
-        align={"center"}
-        h={150}
-        justify="center"
-        bg="howdyColors.mainBlue"
-        flexDir="column"
-        >
-
-        <Text>HELLO</Text>    
-
-        <Flex
-            position={"absolute"}
-            w="60%"
-            h="20"
-            bg={"gray.100"}
-            borderRadius={10}
-            boxShadow='lg'
-            paddingX={20}
-            justify={"space-between"}
-        >
-
-        <Image 
-            width={200}
-            maxWidth={400}
-            src="/images/howdy-images/logo/Logo row.svg"
-            alt="howdy logo">
-        </Image>
-
-        <Center w="50%" justifyContent={"space-between"}>
-            <Text _hover={{color: "howdyColors.mainBlue" }} cursor={"pointer"} color="howdyColors.mainBlack" fontSize={20}>Ranking</Text>
-            <Text _hover={{color: "howdyColors.mainBlue" }} cursor={"pointer"} color="howdyColors.mainBlack" fontSize={20}>Postagens</Text>
-            <Text _hover={{color: "howdyColors.mainBlue" }} cursor={"pointer"} color="howdyColors.mainBlack" fontSize={20}>Aprenda</Text>
-        </Center>
-           
-
-        </Flex>
-     </Flex>
-
-    </>
-
+                    <Center w="50%" justifyContent={'space-between'}>
+                        <Text
+                            _hover={{ color: 'howdyColors.mainBlue' }}
+                            cursor={'pointer'}
+                            color="howdyColors.mainBlack"
+                            fontWeight="semibold"
+                            opacity="70%"
+                            fontSize={22}
+                        >
+                            Ranking
+                        </Text>
+                        <Text
+                            _hover={{ color: 'howdyColors.mainBlue' }}
+                            cursor={'pointer'}
+                            color="howdyColors.mainBlack"
+                            fontWeight="semibold"
+                            opacity="70%"
+                            fontSize={22}
+                        >
+                            Postagens
+                        </Text>
+                        <Text
+                            _hover={{ color: 'howdyColors.mainBlue' }}
+                            cursor={'pointer'}
+                            color="howdyColors.mainBlack"
+                            fontWeight="semibold"
+                            opacity="70%"
+                            fontSize={22}
+                        >
+                            Aprenda
+                        </Text>
+                    </Center>
+                </Flex>
+            </Flex>
+        </>
     );
-
 }

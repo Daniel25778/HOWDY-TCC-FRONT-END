7bb3c167467df35aedd79a128c304c3fe76c7da0
Daniel25778 <danielwilsonca94@gmail.com>
2022-03-21 11:24:59 -0300

    userPage details resolved

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -214,7 +214,10 @@ export default function UserPage(props: any) {
                         </Flex>
                         <Flex>
                            <Flex flexDir="column">
-                                <Text color="howdyColors.mainBlack" fontWeight={'bold'} fontSize={['sm', 'md', 'x-large']}>Helena Pena </Text>
+                                <Flex align="center" gap="1%">
+                                            <Text  color="howdyColors.mainBlack" fontWeight={'bold'} fontSize={['sm', 'md', 'x-large']}>Helena Pena </Text>
+                                            <Text  color="howdyColors.mainBlack" opacity="60%" fontSize={['sm', 'md', 'md']}> ● 19 Nov </Text>
+                                 </Flex>
                                 <Text color="howdyColors.mainBlack" fontSize={['sm', 'md', 'md']} >Amet minim mollit non dese Amet minim mollit non deserunt ullamco est sit</Text>
                                 <IconButton
                                 w="10%"
@@ -238,16 +241,31 @@ export default function UserPage(props: any) {
                     </Flex>
                     <Flex  justify="space-between" align="center"  width="20%">
                         <Flex align="center" gap="10%"  w="20%">
-                            <AiOutlineMessage color="howdyColors.mainBlack" size={30}></AiOutlineMessage>
+                            <IconButton 
+                                    w="10%"
+                                    aria-label="Open navigation"
+                                    bgColor="#ffffff33"
+                                    borderRadius="15"
+                                    icon={<Icon opacity="2" as={AiOutlineMessage} color="howdyColors.mainBlue" fontSize={'x-large'} />}
+                                    >
+                                    </IconButton> 
                             <Text color="howdyColors.mainBlack" fontSize={['sm', 'md', 'md']}>2 mil</Text>
                         </Flex>
                        
                          <Flex gap="10%"  w="20%" align="center">
-                            <AiOutlineHeart color="howdyColors.mainBlack" size={30}></AiOutlineHeart>
+                            <IconButton 
+                                        w="10%"
+                                        aria-label="Open navigation"
+                                        bgColor="#ffffff33"
+                                        borderRadius="15"
+                                        icon={<Icon opacity="2" as={AiOutlineHeart} color="howdyColors.mainBlue" fontSize={'x-large'} />}
+                                        >
+                                        </IconButton> 
                             <Text color="howdyColors.mainBlack" fontSize={['sm', 'md', 'md']}>1 mil</Text>
                          </Flex>
                     </Flex>
                 </Flex>
+
                 <Flex width="100%" align={'center'} mt="5%" flexDir="column">
                     <Flex mb="1%" gap="3%" width="40%" >
                         <Flex>
@@ -261,7 +279,10 @@ export default function UserPage(props: any) {
                         </Flex>
                         <Flex>
                            <Flex flexDir="column">
-                                <Text color="howdyColors.mainBlack" fontWeight={'bold'} fontSize={['sm', 'md', 'x-large']}>Helena Pena </Text>
+                                <Flex align="center" gap="1%">
+                                    <Text  color="howdyColors.mainBlack" fontWeight={'bold'} fontSize={['sm', 'md', 'x-large']}>Helena Pena </Text>
+                                    <Text  color="howdyColors.mainBlack" opacity="60%" fontSize={['sm', 'md', 'md']}> ● 19 Nov </Text>
+                                </Flex>
                                 <Text color="howdyColors.mainBlack" fontSize={['sm', 'md', 'md']} >Amet minim mollit non dese Amet minim mollit non deserunt ullamco est sit</Text>
                                 <IconButton
                                 w="10%"
@@ -285,17 +306,33 @@ export default function UserPage(props: any) {
                     </Flex>
                     <Flex  justify="space-between" align="center"  width="20%">
                         <Flex align="center" gap="10%"  w="20%">
-                            <AiOutlineMessage color="howdyColors.mainBlack" size={30}></AiOutlineMessage>
+                            <IconButton 
+                                    w="10%"
+                                    aria-label="Open navigation"
+                                    bgColor="#ffffff33"
+                                    borderRadius="15"
+                                    icon={<Icon opacity="2" as={AiOutlineMessage} color="howdyColors.mainBlue" fontSize={'x-large'} />}
+                                    >
+                                    </IconButton> 
                             <Text color="howdyColors.mainBlack" fontSize={['sm', 'md', 'md']}>2 mil</Text>
                         </Flex>
                        
                          <Flex gap="10%"  w="20%" align="center">
-                            <AiOutlineHeart color="howdyColors.mainBlack" size={30}></AiOutlineHeart>
+                            <IconButton 
+                                        w="10%"
+                                        aria-label="Open navigation"
+                                        bgColor="#ffffff33"
+                                        borderRadius="15"
+                                        icon={<Icon opacity="2" as={AiOutlineHeart} color="howdyColors.mainBlue" fontSize={'x-large'} />}
+                                        >
+                                        </IconButton> 
                             <Text color="howdyColors.mainBlack" fontSize={['sm', 'md', 'md']}>1 mil</Text>
                          </Flex>
                     </Flex>
                 </Flex>
-<Flex width="100%" align={'center'} mt="5%" flexDir="column">
+
+
+                <Flex width="100%" align={'center'} mt="5%" flexDir="column">
                     <Flex mb="1%" gap="3%" width="40%" >
                         <Flex>
                             <Image
@@ -308,7 +345,10 @@ export default function UserPage(props: any) {
                         </Flex>
                         <Flex>
                            <Flex flexDir="column">
-                                <Text color="howdyColors.mainBlack" fontWeight={'bold'} fontSize={['sm', 'md', 'x-large']}>Helena Pena </Text>
+                                <Flex align="center" gap="1%">
+                                            <Text  color="howdyColors.mainBlack" fontWeight={'bold'} fontSize={['sm', 'md', 'x-large']}>Helena Pena </Text>
+                                            <Text  color="howdyColors.mainBlack" opacity="60%" fontSize={['sm', 'md', 'md']}> ● 19 Nov </Text>
+                                </Flex>
                                 <Text color="howdyColors.mainBlack" fontSize={['sm', 'md', 'md']} >Amet minim mollit non dese Amet minim mollit non deserunt ullamco est sit</Text>
                                 <IconButton
                                 w="10%"
@@ -332,18 +372,95 @@ export default function UserPage(props: any) {
                     </Flex>
                     <Flex  justify="space-between" align="center"  width="20%">
                         <Flex align="center" gap="10%"  w="20%">
-                            <AiOutlineMessage color="howdyColors.mainBlack" size={30}></AiOutlineMessage>
+                            <IconButton 
+                                    w="10%"
+                                    aria-label="Open navigation"
+                                    bgColor="#ffffff33"
+                                    borderRadius="15"
+                                    icon={<Icon opacity="2" as={AiOutlineMessage} color="howdyColors.mainBlue" fontSize={'x-large'} />}
+                                    >
+                                    </IconButton> 
                             <Text color="howdyColors.mainBlack" fontSize={['sm', 'md', 'md']}>2 mil</Text>
                         </Flex>
                        
                          <Flex gap="10%"  w="20%" align="center">
-                            <AiOutlineHeart color="howdyColors.mainBlack" size={30}></AiOutlineHeart>
+                            <IconButton 
+                                        w="10%"
+                                        aria-label="Open navigation"
+                                        bgColor="#ffffff33"
+                                        borderRadius="15"
+                                        icon={<Icon opacity="2" as={AiOutlineHeart} color="howdyColors.mainBlue" fontSize={'x-large'} />}
+                                        >
+                                        </IconButton> 
                             <Text color="howdyColors.mainBlack" fontSize={['sm', 'md', 'md']}>1 mil</Text>
                          </Flex>
                     </Flex>
                 </Flex>
-                
 
+                <Flex width="100%" align={'center'} mt="5%" flexDir="column">
+                    <Flex mb="1%" gap="3%" width="40%" >
+                        <Flex>
+                            <Image
+                                    borderRadius="100%"
+                                    height="5rem"
+                                    objectFit="cover"
+                                    src="/images/Tests/profilePhoto.png"
+                                    alt="profilePhoto"
+                            ></Image>
+                        </Flex>
+                        <Flex>
+                           <Flex flexDir="column">
+                                <Flex align="center" gap="1%">
+                                    <Text  color="howdyColors.mainBlack" fontWeight={'bold'} fontSize={['sm', 'md', 'x-large']}>Helena Pena </Text>
+                                    <Text  color="howdyColors.mainBlack" opacity="60%" fontSize={['sm', 'md', 'md']}> ● 19 Nov </Text>
+                                </Flex>
+                                <Text color="howdyColors.mainBlack" fontSize={['sm', 'md', 'md']} >Amet minim mollit non dese Amet minim mollit non deserunt ullamco est sit</Text>
+                                <IconButton
+                                w="10%"
+                                aria-label="Open navigation"
+                                bgColor="howdyColors.mainBlue"
+                                borderRadius="15"
+                                icon={<Icon opacity="2" as={MdTranslate} color="howdyColors.mainWhite" fontSize={'x-large'} />}
+                                >
+                                </IconButton>
+                           </Flex>
+                        </Flex>
+                    </Flex>
+                    <Flex mb="1%">
+                            <Image
+                                borderRadius="50"
+                                height="30rem"
+                                objectFit="cover"
+                                src="/images/Tests/Rectangle 23.svg"
+                                alt="profilePhoto"
+                            ></Image>
+                    </Flex>
+                    <Flex  justify="space-between" align="center"  width="20%">
+                        <Flex align="center" gap="10%"  w="20%">
+                            <IconButton 
+                                    w="10%"
+                                    aria-label="Open navigation"
+                                    bgColor="#ffffff33"
+                                    borderRadius="15"
+                                    icon={<Icon opacity="2" as={AiOutlineMessage} color="howdyColors.mainBlue" fontSize={'x-large'} />}
+                                    >
+                                    </IconButton> 
+                            <Text color="howdyColors.mainBlack" fontSize={['sm', 'md', 'md']}>2 mil</Text>
+                        </Flex>
+                       
+                         <Flex gap="10%"  w="20%" align="center">
+                            <IconButton 
+                                        w="10%"
+                                        aria-label="Open navigation"
+                                        bgColor="#ffffff33"
+                                        borderRadius="15"
+                                        icon={<Icon opacity="2" as={AiOutlineHeart} color="howdyColors.mainBlue" fontSize={'x-large'} />}
+                                        >
+                                        </IconButton> 
+                            <Text color="howdyColors.mainBlack" fontSize={['sm', 'md', 'md']}>1 mil</Text>
+                         </Flex>
+                    </Flex>
+                </Flex>
             </Box>
         </>
     );

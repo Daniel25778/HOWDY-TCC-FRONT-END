dbf96903a34b3554f8888c7378c6b780c3dbb8e0
GabrielTSR <gabrielalvesrocha22@gmail.com>
2022-03-24 11:30:38 -0300

    firebsae app hsa changed

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -13,12 +13,12 @@ import { getDatabase } from 'firebase/database';
 // };
 
 var firebaseConfig = {
-    apiKey: 'AIzaSyC0TK97o4ONPa4Uzs385gV9rFJ8Y38hkPQ',
-    authDomain: 'howdyproject-a86f8.firebaseapp.com',
-    projectId: 'https://howdyproject-a86f8.firebaseio.com',
-    storageBucket: 'howdyproject-a86f8',
-    messagingSenderId: 'howdyproject-a86f8.appspot.com',
-    appId: '1:111780684034:web:b4bce20e907102e222621b',
+    apiKey: 'AIzaSyCZVKp-Lro_WvFsAsY-UnWUJRBPhJ2j1uc',
+    authDomain: 'howdyservices-19a3b.firebaseapp.com',
+    projectId: 'howdyservices-19a3b',
+    storageBucket: 'howdyservices-19a3b.appspot.com',
+    messagingSenderId: '370892906655',
+    appId: '1:370892906655:web:4b5d43a7f8589f537002aa',
 };
 
 let firebaseApp;

fbab961e32896dd95c1bf8591bc7740d3eea783b
GabrielTSR <gabrielalvesrocha22@gmail.com>
2022-04-03 16:27:43 -0300

    migrating to howdyProject again

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -3,22 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
-// export default {
-//   apiKey: "AIzaSyC0TK97o4ONPa4Uzs385gV9rFJ8Y38hkPQ",
-//   authDomain: "howdyproject-a86f8.firebaseapp.com",
-//   projectId: "https://howdyproject-a86f8.firebaseio.com",
-//   storageBucket: "howdyproject-a86f8",
-//   messagingSenderId: "howdyproject-a86f8.appspot.com",
-//   appId: "1:111780684034:web:b4bce20e907102e222621b",
-// };
-
 var firebaseConfig = {
-    apiKey: 'AIzaSyCZVKp-Lro_WvFsAsY-UnWUJRBPhJ2j1uc',
-    authDomain: 'howdyservices-19a3b.firebaseapp.com',
-    projectId: 'howdyservices-19a3b',
-    storageBucket: 'howdyservices-19a3b.appspot.com',
-    messagingSenderId: '370892906655',
-    appId: '1:370892906655:web:4b5d43a7f8589f537002aa',
+    apiKey: 'AIzaSyC0TK97o4ONPa4Uzs385gV9rFJ8Y38hkPQ',
+    authDomain: 'howdyproject-a86f8.firebaseapp.com',
+    projectId: 'https://howdyproject-a86f8.firebaseio.com',
+    storageBucket: 'howdyproject-a86f8',
+    messagingSenderId: 'howdyproject-a86f8.appspot.com',
+    appId: '1:111780684034:web:b4bce20e907102e222621b',
 };
 
 let firebaseApp;

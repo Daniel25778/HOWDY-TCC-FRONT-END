1a0d0e2b6648d945acfc6349e74121bf64321150
balakelly459 <balakelly459>
2022-04-04 11:03:41 -0300

    input progress

--- src/pages/Posts/[idUser].tsx
+++ src/pages/Posts/[idUser].tsx
@@ -5,19 +5,24 @@ import { api as apiFunction } from '../../services/api';
 import { useEffect, useState } from "react";
 import { getUserLogged } from "../../functions/getUserLogged";
 import Loading from "../../components/Loading/Loading";
+import { Button, Flex, Input, InputGroup, Stack } from "@chakra-ui/react";
+import { FaRegStar } from "react-icons/fa";
 interface PostsProps {
     idUser?: string;
 }
 
 export default function Posts(props:PostsProps){
 
-    const { idUser } = props;
-    const router = useRouter();
     const [userLogged, setUserLogged] = useState<any>(null);
+    const router = useRouter();
+
+    const { idUser } = props;
 
     const api = apiFunction();
 
     const [user, setUser] = useState<any>('nulo');
+
+    const [userPosts, setUserPosts] = useState<any>('nulo');
     
 
     useEffect(() => {
@@ -25,9 +30,19 @@ export default function Posts(props:PostsProps){
             getUserLogged(api).then((res) => {
                 setUserLogged(res);
             });
-                api.get(`users/${idUser}`).then(response => {
-                    response.data && setUser(response.data[0]);
-                });
+
+            //Pegar usuario atraves do id
+
+            api.get(`users/${idUser}`).then(response => {
+                response.data && setUser(response.data[0]);
+            });
+
+            //Pegar postagens do usuario atraves do id
+
+            api.get(`posts/user/${idUser}`).then(response => {
+                response.data && setUserPosts(response.data[0]);
+            })
+
 
         }
     } , [router.isFallback]);
@@ -38,12 +53,27 @@ export default function Posts(props:PostsProps){
         )
       }
 
+
     return (
         <> 
+
+            
+
             <Header user={userLogged}></Header>
-            <Post user={user}></Post>
-            <Post user={user}></Post>
-            <Post user={user}></Post>
+
+           
+            <Flex h="30vh">
+            <Post user={user}userPosts={userPosts}></Post>
+            </Flex>
+
+        <Flex
+            align="center" flexDir="column" p="5%" width="100%" justify="center" borderRadius="60" h="10vh" bgColor={"gray.200"} w="20%">
+            <Stack spacing={3}>
+                <Input width={400} variant="filled" marginBottom="60px" placeholder='Write in English about whatever you want!' borderRadius="100"></Input>
+            </Stack>
+            <Button colorScheme='howdyColors.mainBlue'>Postar</Button>
+
+        </Flex>
         
         </>
     )
